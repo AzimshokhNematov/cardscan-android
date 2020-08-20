a6ade7a8ba1b7ae23cf8644e0a5f8ac805f688a5
dxaen <dinxaen@gmail.com>
2020-08-20 13:21:07 -0700

    model hash

--- scan-payment/src/main/java/com/getbouncer/scan/payment/ml/SSDOcr.kt
+++ scan-payment/src/main/java/com/getbouncer/scan/payment/ml/SSDOcr.kt
@@ -235,7 +235,7 @@ class SSDOcr private constructor(interpreter: Interpreter) :
     class ModelFetcher(context: Context) : UpdatingResourceFetcher(context) {
         override val resource: Int = R.raw.darkniterises
         override val resourceModelVersion: String = "darkniterises"
-        override val resourceModelHash: String = "0ef6e590a5c8b0da63546079a0afacd8ccb72418af68972b72fda45deaca543a"
+        override val resourceModelHash: String = "cdbfc498bf86979448ed98274a81f16c79fdc8230e09da0d73843b7702347bc9"
         override val resourceModelHashAlgorithm: String = "SHA-256"
         override val modelClass: String = "ocr"
         override val modelFrameworkVersion: Int = 1
